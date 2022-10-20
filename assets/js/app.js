56c31ecfa1021169f5da6f5d9bc520457d6bdd67
webcoding_by <webcoding.by@gmail.com>
2022-10-20 16:30:40 +0300

    layout-full-not-mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-console.log('Привет')
+let blockButton = document.querySelector('.block_button');
+let modalWrapper = document.querySelector('.modal_wrapper');
+let modalClose = document.querySelector('.modal_close');
+
+blockButton.addEventListener('click', function(){
+    modalWrapper.classList.add('show');
+})
+
+modalClose.addEventListener('click', function(){
+    modalWrapper.classList.remove('show');
+})
--- index.php
+++ index.php
@@ -10,7 +10,16 @@
 </head>
 <body>
     <div class="wrapper wrap_block">
-        <div class="left_wrap wrap_bgcolor">
+        <div class="left_wrap wrap_bgcolor user_null">
+            <div class="block_user">
+                <h3>Ведите логин:</h3>
+                <div class="block_user--login">
+                    <input id="user_name" type="text" name="login">
+                    <button class="user_button" type="button">Отправить</button>
+                </div>
+            </div>
+        </div>
+        <div class="left_wrap wrap_bgcolor user_enter hidden">
             <div class="block_user">
                 <h3>Ваш баланс:</h3>
                 <div class="block_user--coins">
@@ -18,12 +27,13 @@
                     <span class="price">34</span>
                     <img src="/assets/image/coin.svg" alt="">
                 </div>
+                <button class="user_logout" type="button">Выйти</button>
             </div>
         </div>
         <div class="right_wrap wrap_bgcolor">
             <div class="wrap_block ">
                 <h3 class="block_title">Варианты обмена на скидку</h3>
-                <span class="block_button">Инструкция</span>    
+                <button class="block_button" type="button">Инструкция</button>    
             </div>
             <div class="wrap_block">
                 <div class="products">
@@ -38,7 +48,7 @@
                             <span class="discount">50%</span>
                             <p class="discount_test">на звонки ST(x2)</p>
                         </div>
-                        <div class="button_order active">Уже использовано</div>
+                        <button type="button" class="button_order active">Уже использовано</button>
                     </div>
                     <div class="product">
                         <div class="product_price">
@@ -51,7 +61,7 @@
                             <span class="discount">30%</span>
                             <p class="discount_test">на спецкурс</p>
                         </div>
-                        <div class="button_order">Использовать скидку</div>
+                        <button type="button" class="button_order">Использовать скидку</button>
                     </div>
                     <div class="product">
                         <div class="product_price">
@@ -64,7 +74,7 @@
                             <span class="discount">50%</span>
                             <p class="discount_test">на курс</p>
                         </div>
-                        <div class="button_order">Использовать скидку</div>
+                        <button type="button" class="button_order">Использовать скидку</button>
                     </div>
                     <div class="product">
                         <div class="product_price">
@@ -77,8 +87,22 @@
                             <span class="discount">65%</span>
                             <p class="discount_test">на курс</p>
                         </div>
-                        <div class="button_order">Использовать скидку</div>
+                        <button type="button" class="button_order">Использовать скидку</button>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal_wrapper">
+        <div class="modal_inner">
+            <button type="button" class="modal_close">X</button>
+            <div class="modal">
+                <h3>Инструкция</h3>
+                <div class="modal_text">
+                    <p>После публикации сделайте скрин, что вы его написали и отправьте своему куратору, чтобы мы добавили вам спецкурс в личный кабинет.</p>
+                    <p>После публикации сделайте скрин, что вы его написали и отправьте своему куратору, чтобы мы добавили вам спецкурс в личный кабинет.</p>
+                    <p>После публикации сделайте скрин, что вы его написали и отправьте своему куратору, чтобы мы добавили вам спецкурс в личный кабинет.</p>
+                    <p>После публикации сделайте скрин, что вы его написали и отправьте своему куратору, чтобы мы добавили вам спецкурс в личный кабинет.</p>
                 </div>
             </div>
         </div>
