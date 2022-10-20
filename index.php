3377b8bba053307ebac79b589eeecef41ef7c97b
webcoding_by <webcoding.by@gmail.com>
2022-10-20 15:13:20 +0300

    layout

--- index.php
+++ index.php
@@ -4,9 +4,85 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Тендер</title>
+    <title>English</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="shortcut icon" href="/assets/image/money.png" type="image/x-icon">
 </head>
 <body>
-    
+    <div class="wrapper wrap_block">
+        <div class="left_wrap wrap_bgcolor">
+            <div class="block_user">
+                <h3>Ваш баланс:</h3>
+                <div class="block_user--coins">
+                    <img class="coins" src="/assets/image/money.svg" alt="монета">
+                    <span class="price">34</span>
+                    <img src="/assets/image/coin.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="right_wrap wrap_bgcolor">
+            <div class="wrap_block ">
+                <h3 class="block_title">Варианты обмена на скидку</h3>
+                <span class="block_button">Инструкция</span>    
+            </div>
+            <div class="wrap_block">
+                <div class="products">
+                    <div class="product">
+                        <div class="product_price">
+                            <img src="/assets/image/money.svg" alt="">
+                            <span class="price">50</span>
+                            <img src="/assets/image/coin.svg" alt="">
+                        </div>
+                        <img class="product_img" src="/assets/image/phone.svg" alt="">
+                        <div class="discount_desc">
+                            <span class="discount">50%</span>
+                            <p class="discount_test">на звонки ST(x2)</p>
+                        </div>
+                        <div class="button_order active">Уже использовано</div>
+                    </div>
+                    <div class="product">
+                        <div class="product_price">
+                            <img src="/assets/image/money.svg" alt="">
+                            <span class="price">70</span>
+                            <img src="/assets/image/coin.svg" alt="">
+                        </div>
+                        <img class="product_img" src="/assets/image/book.svg" alt="">
+                        <div class="discount_desc">
+                            <span class="discount">30%</span>
+                            <p class="discount_test">на спецкурс</p>
+                        </div>
+                        <div class="button_order">Использовать скидку</div>
+                    </div>
+                    <div class="product">
+                        <div class="product_price">
+                            <img src="/assets/image/money.svg" alt="">
+                            <span class="price">100</span>
+                            <img src="/assets/image/coin.svg" alt="">
+                        </div>
+                        <img class="product_img" src="/assets/image/books.svg" alt="">
+                        <div class="discount_desc">
+                            <span class="discount">50%</span>
+                            <p class="discount_test">на курс</p>
+                        </div>
+                        <div class="button_order">Использовать скидку</div>
+                    </div>
+                    <div class="product">
+                        <div class="product_price">
+                            <img src="/assets/image/money.svg" alt="">
+                            <span class="price">139</span>
+                            <img src="/assets/image/coin.svg" alt="">
+                        </div>
+                        <img class="product_img" src="/assets/image/books.svg" alt="">
+                        <div class="discount_desc">
+                            <span class="discount">65%</span>
+                            <p class="discount_test">на курс</p>
+                        </div>
+                        <div class="button_order">Использовать скидку</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="/assets/js/app.js"></script>
 </body>
 </html>
